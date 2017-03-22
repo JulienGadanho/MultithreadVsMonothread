72e54977119171ab7cb74c41bf197e592eda978b
JulienGadanho <jgadanho.ads@gmail.com>
2017-03-22 02:44:37 +0100

    comparatif

---
+++ comparator.php
@@ -0,0 +1,67 @@
+<?php
+
+$urls = array("http://www.meilleurtaux.com/rachat-de-credits/index.html","http://www.empruntis.com/rachat-credits/","https://www.cofidis.fr/fr/rachat-de-credit/simulation-rachat-de-credit.html","https://www.boursedescredits.com/rachat-credits.php","http://www.cofinoga.fr/credit/rachat-credits","https://www.sofinco.fr/credit-pret/rachat-credit.htm","http://www.cetelem.fr/credit/rachat-credits/","https://www.solutis.fr/","https://www.labanquepostale.fr/particulier/produits/credits_consommation/rachat_credits.avantages.html","https://www.bioam.fr/rachat-de-credit/","https://www.younited-credit.com/projets/rachat-de-credit","https://www.oney.fr/site/b/credits/regroupement-de-credit.html","https://www.labanquepostale.fr/particulier/Outils/simulateurs/simulateur_rachat_credit.rc.html","http://credit.gemoneybank.fr/fr/rachat-de-credits.html","http://www.financo.fr/credit/financement/avantage-rachat-credit","http://www.cetelem.fr/credit/pret-personnel/projet-avec-rachat-credit/","http://www.cetelem.fr/credit/rachat-credits/sans-credit-immobilier/","https://www.solutis.fr/simulation-rachat-credit.html","https://www.franfinance.fr/le-rachat-de-credits-le-bon-reflexe-budget/","https://www.banque-casino.fr/credits/simulation-rachat-credit","http://www.cofinoga.fr/credit/rachat-credit/fonctionnement","https://www.cofidis.fr/fr/rachat-de-credit.html","https://www.cofidis.fr/fr/rachat-de-credit/guide/fonctionnement-rachat-credit.html","http://www.inandfi-credits.fr/rachat-de-credit/","https://www.excellia-finance.fr/rachat-de-credits/","https://www.empruntis.com/rachat-de-credit/comparateur-rachat-credit/comparateur-rachat-credit.php","http://www.meilleurtaux.com/rachat-de-credits/rachat-de-credits-comment/rachat-de-credits-et-financement-de-nouveaux-projets/","http://www.meilleurtaux.com/rachat-de-credits/simulations-de-rachat-de-credits/","https://www.creditmutuel.fr/cmag/fr/banques/particuliers/quotidien/maitriser-vos-imprevus/rachat-de-credit-simulation.html","http://www.capital.fr/finances-perso/dossiers/rachat-de-credit-une-solution-de-secours-a-envisager-avec-grande-prudence-1118681","https://www.responis.fr/simulation-rachat-credit.html","https://www.solutis.fr/demande-rachat-credit.html","http://www.empruntis.com/rachat-credits/mon-projet-de-refinancement/","https://particuliers.societegenerale.fr/emprunter/credits-conso/regroupement-credit.html","https://www.cofidis.fr/fr/rachat-de-credit/guide/avantages-rachat-de-credit.html","https://www.cofidis.fr/fr/rachat-de-credit/guide/dossier-rachat-credit.html","http://www.meilleurtaux.com/rachat-de-credits/le-guide-du-rachat-de-credits/cout-rachat-credits.html","http://www.meilleurtaux.com/credit-immobilier/le-guide-de-l-emprunteur/rachat-de-pret-renegocier-son-credit.html","https://www.meilleurtaux.com/demande-simulation/rachat-de-credits/","http://www.credit-municipal-lyon.fr/rachat-credit-lyon.php","https://www.cofidis.fr/fr/rachat-de-credit/guide/etapes-rachat-credit.html","https://www.maif.fr/particuliers/epargne-financement/restructuration-de-credit/rachat-de-credit.html","http://www.vousfinancer.com/rachat-credit/","https://fr.wikipedia.org/wiki/Rachat_de_crédit","https://www.creditmutuel.fr/cmag/fr/banques/particuliers/quotidien/maitriser-vos-imprevus/rachat-de-credit-avantages.html","https://www.creditmutuel.fr/fr/simulations/credit/regroupement.html","http://www.panorabanques.com/rachat-de-credit","https://rachat-credit.ooreka.fr/","http://www.ingdirect.fr/credit-immobilier/rachat-de-credit-immobilier/","https://www.service-public.fr/particuliers/vosdroits/F146","http://www.cafpi.fr/nos-offres-rachat-credit-immobilier.php","https://patrimoine.lesechos.fr/immobilier/credit/0211576806309-renegociation-ou-rachat-de-credit-il-est-encore-temps-2054915.php","https://www.araf.fr/","https://www.cofidis.fr/","http://www.cetelem.fr/credit/rachat-credits/avec-credit-immobilier/","http://www.cbanque.com/credit/rachat-de-credits.php","http://www.credit-municipal-nimes.fr/fr/prets/rachat-de-credits","https://www.credit-municipal-bordeaux.fr/pret-personnel/calculette/rachat.html","http://www.credigo.fr/simulation-rachat-credit.html","https://www.keyliance.fr/","https://www.finaprets.com/","https://www.cofidis.fr/fr/rachat-de-credit/guide/rachat-de-credit-consommation.html","http://nouveaupret.com/rachat-de-credit.html","https://www.eig-finances.com/rachat-credits/","http://www.meilleurtaux.com/rachat-de-credits/le-guide-du-rachat-de-credits/questions-reponses-rachat-credits/","http://www.finanzen.fr/credit-epargne/rachat-de-credit","https://www.responis.fr/","https://www.csf.fr/jcms/mle_835/rachat-de-credits","http://www.empruntis.com/rachat-credits/mon-projet-de-refinancement/pourquoi-faire-un-rachat-de-credits/le-rachat-de-credits-definition.php","https://www.ca-normandie.fr/magazine/argent/le-rachat-de-credit-comment-ca-fonctionne.html","https://www.quechoisir.org/actualite-rachat-de-credit-une-offre-sur-cinq-en-infraction-n3627/","http://www.cetelem.fr/credit/simulation/projet-avec-rachat-credit/","https://www.cofidis.fr/fr/rachat-de-credit/guide.html","http://reassurez-moi.fr/rachat-credit-immobilier","https://www.groupama.fr/banque/rachats-credits.html","https://www.bcfinance.fr/","https://www.solutis.fr/rachat-credit-consommation.html","https://www.labanquepostale.fr/particulier/produits/pret_immo/rachat_credits.rpi.html","http://www.cafpi.fr/nos-offres-rachat-soulte.php","https://www.responis.fr/demande-rachat-credit.html","http://www.cetelem.fr/guide-credit/rachat-credits-conso/","https://www.sofinco.fr/simulation-credit/rachat-credit.htm","https://www.solutis.fr/rachat-credit-fonctionnaire.html","https://www.credit-municipal-bordeaux.fr/pret-personnel/calculette/cnas_rachat.html","http://www.boursorama.com/aide/assistance/credit/credit-personnel/comment-se-passe-un-rachat-de-credit-par-la-concurrence--1641","https://particuliers.lcl.fr/prets/prets-personnels/rachat-de-pret/","http://www.meilleurtaux.com/credit-immobilier/simulation-de-pret-immobilier/renegociez-votre-credit-immobilier.html","http://www.meilleurtaux.com/credit-immobilier/le-guide-de-l-emprunteur/rachat-de-pret-renegocier-son-credit/documents-a-fournir.html","http://www.lepetitpoucetducredit.com/","http://www.empruntis.com/rachat-credits/mon-projet-de-refinancement/pourquoi-faire-un-rachat-de-credits/","http://www.vousfinancer.com/rachat-credit/le-rachat.html","http://credit.gemoneybank.fr/fr/regroupement-de-credit/regrouper-credits-consommation.html","https://www.solutis.fr/rachat-credit-senior-retraite.html","https://www.solutis.fr/rachat-credit-faible-taux.html","https://www.solutis.fr/rachat-credit-france.html","http://www.empruntis.com/rachat-credits/actualites/","http://www.empruntis.com/","http://www.cofinoga.fr/","https://www.ca-centrefrance.fr/magazine/argent/le-rachat-de-credit-comment-ca-fonctionne.html","http://www.meilleurtaux.com/rachat-de-credits/simulations-de-rachat-de-credits/regrouper-vos-credits-consommation.html");
+
+echo "\n\n\n";
+echo "Start multithread\n";
+
+$timestamp_debut = microtime(true);
+#-------------------------------------------------------------------
+$mh = curl_multi_init();
+
+foreach($urls as $key=> $url) {
+    
+    //echo "ADD $url\n";
+    $ch_{$key} = curl_init($url);
+    curl_setopt($ch_{$key}, CURLOPT_RETURNTRANSFER, true);
+    curl_multi_add_handle($mh, $ch_{$key});
+}
+
+$active = null;
+//execute the handles
+do {
+    $mrc = curl_multi_exec($mh, $active);
+} while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+while ($active && $mrc == CURLM_OK) {
+    if (curl_multi_select($mh) != -1) {
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+    }
+}
+
+
+foreach($urls as $key=> $url) {
+    
+    //close the handles
+    curl_multi_remove_handle($mh, $ch_{$key});
+}
+
+curl_multi_close($mh);
+ 
+ #-------------------------------------------------------------------
+$timestamp_fin = microtime(true);
+$difference_ms = $timestamp_fin - $timestamp_debut;
+echo 'Exécution du script multithread : ' . $difference_ms . ' secondes.' . "\n";
+
+
+echo "\n\n\n";
+echo "Start monothread\n";
+
+$timestamp_debut = microtime(true);
+#-------------------------------------------------------------------
+$mh = curl_multi_init();
+
+foreach($urls as $key=> $url) {
+    
+    //echo "WORK ON $url\n";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+}
+ 
+ #-------------------------------------------------------------------
+$timestamp_fin = microtime(true);
+$difference_ms = $timestamp_fin - $timestamp_debut;
+echo 'Exécution du script monothread : ' . $difference_ms . ' secondes.'. "\n";
